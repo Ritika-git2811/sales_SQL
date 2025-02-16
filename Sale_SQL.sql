be1d07a7ea512f39f48e4393b8977c7b02163cc4
Ritika Gupta <ritikagupta2811@gmail.com>
2025-02-16 19:48:15 +0530

    Add files via upload

--- Sale_SQL.sql
+++ Sale_SQL.sql
@@ -1,13 +1,15 @@
-#Determine the top 3 most ordered pizza types based on revenue for each pizza category.
-select name, revenue from
-(select category, name, revenue,
-rank() over(partition by category order by revenue desc) as rn
-from
-(select pizza_types.category, pizza_types.name,
-sum((order_details.quantity)*pizzas.price)as revenue 
+#Calculate the percentage contribution of each pizza type to total revenue.
+select pizza_types.category,
+round(sum(order_details.quantity*pizzas.price)/(SELECT 
+    ROUND(SUM(order_details.quantity * pizzas.price),
+            2) AS total_sales
+FROM
+    order_details
+        JOIN
+    pizzas ON pizzas.pizza_id = order_details.pizza_id)*100,0) as revenue
+
 from pizza_types join pizzas
-on pizza_types.pizza_type_id= pizzas.pizza_type_id
+on pizza_types.pizza_type_id=pizzas.pizza_type_id 
 join order_details
-on order_details.pizza_id =pizzas.pizza_id
-group by pizza_types.category, pizza_types.name)as a)as b
-where rn<=3;
+on order_details.pizza_id=pizzas.pizza_id
+group by pizza_types.category order by revenue desc;
